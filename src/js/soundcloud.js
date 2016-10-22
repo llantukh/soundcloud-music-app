ba51ee6f84aa21aad3d8e4272c1be9cb16918f42
Liliya Lantukh <liliya.lantukh@gmail.com>
2016-10-22 19:21:21 -0400

    created ajax request

---
+++ src/js/soundcloud.js
@@ -0,0 +1,30 @@
+import $ from 'jquery';
+import clientId from "./token.js";
+
+console.log(clientId);
+
+//create ajax setup for the client id since it has to be in the url
+
+$.ajaxSetup ({
+  data: {
+    client_id: clientId
+  }
+});
+
+
+//make ajax request:
+
+var makeRequest = $.ajax({
+  url: "https://api.soundcloud.com/tracks"
+});
+
+
+//make sure the request works and returns something
+//
+// function logData (data) {
+//   console.log(data);
+// };
+//
+
+
+export {makeRequest};
