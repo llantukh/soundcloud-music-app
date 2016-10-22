91efc5fbaf7e4a2d08ba4cb149eba2575fd7a2d8
Liliya Lantukh <liliya.lantukh@gmail.com>
2016-10-22 19:24:06 -0400

    pulled appropriate data from ajax result and created the html to get it onto the page

---
+++ src/js/template.js
@@ -0,0 +1,23 @@
+import $ from "jquery"
+
+
+function displayResults (data){
+
+data.forEach(function(item){
+  console.log(item)
+  var resultsHtml = `<div class = "resultsBox">
+    <img src = ${item.artwork_url || item.user.avatar_url}>
+    <div class = "songName">
+      <span class = "songTitle"> ${item.title} <span>
+    </div>
+    <div class = "artistName">
+      <span class = "artist"> ${item.user.username} <span>
+    </div>
+  </div>`
+
+  $(".searchResults").append(resultsHtml);
+
+})
+};
+
+export { displayResults };
